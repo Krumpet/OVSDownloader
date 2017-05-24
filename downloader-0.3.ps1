1f547d385a7c140f610b855cbb9f80be6e17f1e8
Krumpet <ran.lottem@campus.technion.ac.il>
2017-05-24 13:20:18 +0300

    Second commit

---
+++ downloader-0.3.ps1
@@ -0,0 +1,79 @@
+$server="https://video.technion.ac.il" # Technion video server
+
+$progressPreference = 'silentlyContinue' # For wget not to bother the user (similar to wget -q)
+
+Write-Output "Old Technion Video Server Downloader v0.3 by Ran Lottem"
+Write-Output "Based on bash script by Ohad Eytan"
+
+# Check dependency
+try {
+    & .\msdl.exe -qh
+} catch {
+	Write-Output "Error: msdl.exe not found in this script's folder."
+	exit
+}
+
+# Get user input
+$Course = Read-Host -Prompt 'Please insert valid course url (e.g https://video.technion.ac.il/Courses/PhysMech.html).'
+$User = Read-Host -Prompt 'Please insert username (e.g ran.lottem without @campus etc.)'
+$Pass = Read-Host -Prompt 'Please insert password' # TODO: Set an option to set user/pw once and remember
+
+# Get URL
+$params = @{username="$User";password="$Pass"}
+$URL = Invoke-WebRequest $Course -Method POST -Body $params
+
+# Check URL
+if (-not $?) {
+	Write-Output "Error: please provide course page link. (e.g http://video.technion.ac.il/Courses/PhysMech.html)"
+	exit
+}
+
+# Get links and amount of links
+$links = $URL.Links | Select-Object href | grep -a movies/rtsp
+$num = ($links | Measure-Object).count
+
+# Check amount
+if ($num -le 0) {
+	Write-Output "Error: no files found."
+	exit
+}
+
+# Get file range for download
+Write-Host "There are $num files."
+$start = Read-Host -Prompt "Please insert first video number (1-$num)"
+while (($start -lt 1) -or ($start -gt $num)) {
+    $start = Read-Host -Prompt "Invalid start. Please insert first video number (1-$num)"
+}
+
+$end = Read-Host -Prompt "Please insert last video number ($start-$num)"
+while (($end -lt $start) -or ($end -gt $num)) {
+    $end = Read-Host -Prompt "Invalid end. Please insert last video number ($start-$num)"
+}
+
+# Trim links list
+$links = $links | Select-Object -Skip ($start-1) | Select-Object -SkipLast ($num-$end)
+
+# Let's rock!
+foreach ($line in $links) {
+	$filename = $line.split("/")[5]
+    $filename = $filename.Trim() # filename for saving
+    # TODO: Check if file exists, skip it
+    $file2 = Invoke-WebRequest -uri "$server$line" -Method POST -Body $params
+	$address = ($file2.Content | grep -a window.location=).split("`"")[1]
+    cmd /c .\msdl.exe -s2 $address -o $filename '2>&1' | ForEach-Object {
+        $first=1
+        $newline=0
+        if ($_.startswith("DL")) {
+            Write-Host -NoNewline "`r$_"
+            $first=0
+            $newline=0
+        }
+        else {
+            if ((-not $first) -and (-not $newline)) {
+                Write-Output "" # for newline between files
+                $newline=1
+            }
+            Write-Output "$_"
+        }
+    }
+}
